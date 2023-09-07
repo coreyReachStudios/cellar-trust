b75cac2beec8725b408fb37f5ebc262e6d2114e0
coreyfielding <corey.fielding@reachstudios.co.uk>
2023-09-07 16:16:41 +0100

    page builder

--- src/Pagebuilder/Blocks/Hero.tsx
+++
@@ -1,5 +0,0 @@
-import { Page_Pagebuilder_PageBlocks_HeroBlock } from "@/utils/_generated/graphql";
-
-const Hero = ({ helpOptions, heroImage, subtitle, title }: Page_Pagebuilder_PageBlocks_HeroBlock) => {
-	return <></>;
-};
